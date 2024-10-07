dbc24db42ae3e3ab00af4eb04afa2ce3ed6796fa
IgorKa45 <igorkanasnik@gmail.com>
2024-10-07 13:11:44 +0400

    Сделал дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String space = " ";
+        String fullName = lastName + space + firstName + space + middleName;
+        System.out.println(fullName);
+
+        System.out.println("Задание 2");
+        System.out.println(fullName.toUpperCase());
+
+        System.out.println("Задание 3");
+        String fullName2 = "Иванов Семён Семёнович";
+        fullName2 = fullName2.replace('ё', 'e');
+        System.out.println(fullName2);
     }
 }
