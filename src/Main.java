e960cd89ad7395820c51bb912a2a877dbceec5a5
IgorKa45 <igorkanasnik@gmail.com>
2024-10-07 13:14:46 +0400

    Сделал дз 2

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,14 @@ public class Main {
         String lastName = "Ivanov";
         String space = " ";
         String fullName = lastName + space + firstName + space + middleName;
-        System.out.println(fullName);
+        System.out.println("Ф.И.О. сотрудника — " + fullName);
 
         System.out.println("Задание 2");
-        System.out.println(fullName.toUpperCase());
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
 
         System.out.println("Задание 3");
         String fullName2 = "Иванов Семён Семёнович";
         fullName2 = fullName2.replace('ё', 'e');
-        System.out.println(fullName2);
+        System.out.println("Данные Ф.И.О. сотрудника — " + fullName2);
     }
 }
